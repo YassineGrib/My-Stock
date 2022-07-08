20cf1357937ad27090b4083fd6f4bca3146f559e
Admin <Admin@LENOVO>
2022-07-08 19:07:28 +0100

    Add project files.

---
+++ My Stock/Forms/Form1.Designer.vb
@@ -0,0 +1,247 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Login))
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.UsernameComboBox = New Guna.UI2.WinForms.Guna2ComboBox()
+        Me.LoginButton = New Guna.UI2.WinForms.Guna2Button()
+        Me.Guna2VSeparator1 = New Guna.UI2.WinForms.Guna2VSeparator()
+        Me.Guna2PictureBox4 = New Guna.UI2.WinForms.Guna2PictureBox()
+        Me.Guna2PictureBox3 = New Guna.UI2.WinForms.Guna2PictureBox()
+        Me.Guna2PictureBox2 = New Guna.UI2.WinForms.Guna2PictureBox()
+        Me.PasswordTextBox = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.Guna2PictureBox1 = New Guna.UI2.WinForms.Guna2PictureBox()
+        CType(Me.Guna2PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Guna2PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Guna2PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI Semibold", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.Label3.Location = New System.Drawing.Point(1237, 554)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(142, 23)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "Forget Passowrd!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.Label2.Location = New System.Drawing.Point(605, 469)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(80, 23)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Password"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(605, 357)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 23)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "User name"
+        '
+        'UsernameComboBox
+        '
+        Me.UsernameComboBox.BackColor = System.Drawing.Color.Transparent
+        Me.UsernameComboBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.UsernameComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.UsernameComboBox.FocusedColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.UsernameComboBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.UsernameComboBox.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.UsernameComboBox.ForeColor = System.Drawing.Color.Black
+        Me.UsernameComboBox.ItemHeight = 40
+        Me.UsernameComboBox.Location = New System.Drawing.Point(604, 382)
+        Me.UsernameComboBox.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.UsernameComboBox.Name = "UsernameComboBox"
+        Me.UsernameComboBox.Size = New System.Drawing.Size(775, 46)
+        Me.UsernameComboBox.TabIndex = 10
+        Me.UsernameComboBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'LoginButton
+        '
+        Me.LoginButton.Animated = True
+        Me.LoginButton.BackColor = System.Drawing.Color.RoyalBlue
+        Me.LoginButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.LoginButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.LoginButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.LoginButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.LoginButton.FillColor = System.Drawing.Color.RoyalBlue
+        Me.LoginButton.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LoginButton.ForeColor = System.Drawing.Color.White
+        Me.LoginButton.Location = New System.Drawing.Point(604, 645)
+        Me.LoginButton.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.LoginButton.Name = "LoginButton"
+        Me.LoginButton.Size = New System.Drawing.Size(775, 58)
+        Me.LoginButton.TabIndex = 9
+        Me.LoginButton.Text = "Login"
+        '
+        'Guna2VSeparator1
+        '
+        Me.Guna2VSeparator1.Location = New System.Drawing.Point(546, 109)
+        Me.Guna2VSeparator1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Guna2VSeparator1.Name = "Guna2VSeparator1"
+        Me.Guna2VSeparator1.Size = New System.Drawing.Size(12, 560)
+        Me.Guna2VSeparator1.TabIndex = 18
+        '
+        'Guna2PictureBox4
+        '
+        Me.Guna2PictureBox4.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2PictureBox4.Image = Global.My_Stock.My.Resources.Resources.logo
+        Me.Guna2PictureBox4.ImageRotate = 0!
+        Me.Guna2PictureBox4.Location = New System.Drawing.Point(788, 85)
+        Me.Guna2PictureBox4.Margin = New System.Windows.Forms.Padding(4)
+        Me.Guna2PictureBox4.Name = "Guna2PictureBox4"
+        Me.Guna2PictureBox4.Size = New System.Drawing.Size(407, 271)
+        Me.Guna2PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Guna2PictureBox4.TabIndex = 20
+        Me.Guna2PictureBox4.TabStop = False
+        Me.Guna2PictureBox4.UseTransparentBackground = True
+        '
+        'Guna2PictureBox3
+        '
+        Me.Guna2PictureBox3.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2PictureBox3.Image = Global.My_Stock.My.Resources.Resources.login_image1
+        Me.Guna2PictureBox3.ImageRotate = 0!
+        Me.Guna2PictureBox3.Location = New System.Drawing.Point(82, 577)
+        Me.Guna2PictureBox3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Guna2PictureBox3.Name = "Guna2PictureBox3"
+        Me.Guna2PictureBox3.Size = New System.Drawing.Size(391, 126)
+        Me.Guna2PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Guna2PictureBox3.TabIndex = 19
+        Me.Guna2PictureBox3.TabStop = False
+        Me.Guna2PictureBox3.UseTransparentBackground = True
+        '
+        'Guna2PictureBox2
+        '
+        Me.Guna2PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2PictureBox2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Guna2PictureBox2.Image = Global.My_Stock.My.Resources.Resources.Close
+        Me.Guna2PictureBox2.ImageRotate = 0!
+        Me.Guna2PictureBox2.Location = New System.Drawing.Point(1376, 15)
+        Me.Guna2PictureBox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Guna2PictureBox2.Name = "Guna2PictureBox2"
+        Me.Guna2PictureBox2.Size = New System.Drawing.Size(52, 52)
+        Me.Guna2PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Guna2PictureBox2.TabIndex = 16
+        Me.Guna2PictureBox2.TabStop = False
+        Me.Guna2PictureBox2.UseTransparentBackground = True
+        '
+        'PasswordTextBox
+        '
+        Me.PasswordTextBox.Animated = True
+        Me.PasswordTextBox.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.PasswordTextBox.DefaultText = ""
+        Me.PasswordTextBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.PasswordTextBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.PasswordTextBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.PasswordTextBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.PasswordTextBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.PasswordTextBox.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        Me.PasswordTextBox.ForeColor = System.Drawing.Color.Black
+        Me.PasswordTextBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.PasswordTextBox.IconLeftSize = New System.Drawing.Size(32, 32)
+        Me.PasswordTextBox.IconRight = Global.My_Stock.My.Resources.Resources.Show
+        Me.PasswordTextBox.IconRightOffset = New System.Drawing.Point(3, 0)
+        Me.PasswordTextBox.IconRightSize = New System.Drawing.Size(25, 25)
+        Me.PasswordTextBox.Location = New System.Drawing.Point(604, 494)
+        Me.PasswordTextBox.Margin = New System.Windows.Forms.Padding(4, 2, 4, 2)
+        Me.PasswordTextBox.Name = "PasswordTextBox"
+        Me.PasswordTextBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.PasswordTextBox.PlaceholderText = ""
+        Me.PasswordTextBox.SelectedText = ""
+        Me.PasswordTextBox.Size = New System.Drawing.Size(775, 58)
+        Me.PasswordTextBox.TabIndex = 11
+        Me.PasswordTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Guna2PictureBox1
+        '
+        Me.Guna2PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2PictureBox1.Image = CType(resources.GetObject("Guna2PictureBox1.Image"), System.Drawing.Image)
+        Me.Guna2PictureBox1.ImageRotate = 0!
+        Me.Guna2PictureBox1.Location = New System.Drawing.Point(41, 30)
+        Me.Guna2PictureBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Guna2PictureBox1.Name = "Guna2PictureBox1"
+        Me.Guna2PictureBox1.Size = New System.Drawing.Size(471, 598)
+        Me.Guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.Guna2PictureBox1.TabIndex = 0
+        Me.Guna2PictureBox1.TabStop = False
+        Me.Guna2PictureBox1.UseTransparentBackground = True
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(120.0!, 120.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1444, 778)
+        Me.Controls.Add(Me.Guna2PictureBox4)
+        Me.Controls.Add(Me.Guna2PictureBox3)
+        Me.Controls.Add(Me.Guna2VSeparator1)
+        Me.Controls.Add(Me.Guna2PictureBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PasswordTextBox)
+        Me.Controls.Add(Me.UsernameComboBox)
+        Me.Controls.Add(Me.LoginButton)
+        Me.Controls.Add(Me.Guna2PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Login"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Log in"
+        CType(Me.Guna2PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Guna2PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Guna2PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Guna2PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Guna2PictureBox1 As Guna.UI2.WinForms.Guna2PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PasswordTextBox As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents UsernameComboBox As Guna.UI2.WinForms.Guna2ComboBox
+    Friend WithEvents LoginButton As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Guna2PictureBox2 As Guna.UI2.WinForms.Guna2PictureBox
+    Friend WithEvents Guna2VSeparator1 As Guna.UI2.WinForms.Guna2VSeparator
+    Friend WithEvents Guna2PictureBox3 As Guna.UI2.WinForms.Guna2PictureBox
+    Friend WithEvents Guna2PictureBox4 As Guna.UI2.WinForms.Guna2PictureBox
+End Class
---
+++ My Stock/Forms/Form1.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Login
+    ReadOnly SQLCONN As New SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\MVS\My Stock\My Stock\Database\MSDB.mdf;Integrated Security=True")
+    Private Sub Guna2PictureBox2_Click(sender As Object, e As EventArgs) Handles Guna2PictureBox2.Click
+        'the exit Button
+        End
+    End Sub
+
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Make Password crypted 
+        PasswordTextBox.UseSystemPasswordChar = True
+
+    End Sub
+    Private Sub UsernameComboBox_GotFocus(sender As Object, e As EventArgs) Handles UsernameComboBox.GotFocus
+        'fill Username ComboBox from DB  
+        Dim SQLCMD As New SqlCommand($"select Username from User_TBL", SQLCONN)
+        Dim adapter As New SqlDataAdapter(SQLCMD)
+        Dim table As New DataTable()
+        adapter.Fill(table)
+        UsernameComboBox.DataSource = table
+        UsernameComboBox.DisplayMember = "Username"
+        UsernameComboBox.ValueMember = "Username"
+    End Sub
+
+
+
+    Private Sub PasswordTextBox_IconRightClick(sender As Object, e As EventArgs) Handles PasswordTextBox.IconRightClick
+        'Textbox password right icon (Show/hide Password)
+        If PasswordTextBox.UseSystemPasswordChar = True Then
+            PasswordTextBox.UseSystemPasswordChar = False
+        Else
+            PasswordTextBox.UseSystemPasswordChar = True
+        End If
+
+    End Sub
+
+    Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+        'Chack for Password of username Selected 
+
+        If PasswordTextBox.Text = String.Empty Then
+            MsgBox("Please entry the password!", MsgBoxStyle.Exclamation)
+
+        ElseIf UsernameComboBox.Text = String.Empty Then
+            MsgBox("Please select the User!", MsgBoxStyle.Exclamation)
+        Else
+            Dim SQLCMD As New SqlCommand($"select Username,Password from User_TBL where username='{UsernameComboBox.Text}' and Password='{PasswordTextBox.Text}'", SQLCONN)
+            Dim adapter As New SqlDataAdapter(SQLCMD)
+            Dim table As New DataTable()
+            adapter.Fill(table)
+            If table.Rows.Count = 0 Then
+                MsgBox("Wrong Password!", MsgBoxStyle.Exclamation)
+            Else
+                MsgBox("Password Correct", MsgBoxStyle.Information)
+
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        MsgBox("Ohh we sorry for that! Please contact your IT ADMIN", MsgBoxStyle.Information)
+
+    End Sub
+End Class
